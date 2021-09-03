2f10a4cbf88203c5337c6f8cf52b1f4698512a11
Vignesh Mahadevan <vigneshmr13@outlook.com>
2021-09-03 15:23:34 +0530

    First Commit

---
+++ app/src/main/java/in/apexcoders/project_moon/MainActivity.java
@@ -0,0 +1,14 @@
+package in.apexcoders.project_moon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
